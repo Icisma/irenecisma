c4ff063a0c69d915d9194e35317ac30f51d1b0f3
Icisma <158587501+Icisma@users.noreply.github.com>
2026-05-13 17:08:24 +0000

    Deploying to gh-pages from @ Icisma/irenecisma@ca2ab86225973966676ec183626577824d13003f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,35 +518,49 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%69%72%65%6E%65@%69%72%65%6E%65%63%69%73%6D%61.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/0009-0003-3275-1124", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=jzxOPYAAAAAJ&hl", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-bluesky',
+        title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://bsky.app/profile/irenecisma.bsky.social", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/irecisma", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/irenecisma", "_blank");
+        },
+      },{
+        id: 'social-pinterest',
+        title: 'Pinterest',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.pinterest.com/cidiaz7", "_blank");
         },
       },{
       id: 'light-theme',
